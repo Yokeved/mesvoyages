13e579c00b07e73040222c2a37102855893cad5c
Yoheved Levy <yohevedlevy23@gmail.com>
2023-04-16 18:55:16 +0200

    Phase 4 : Plusieurs pages

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -8,24 +8,24 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * Description of AccueilController
+ * Description of VoyagesController
  *
  * @author Yokheved
  */
-class AccueilController extends AbstractController{
+class VoyagesController extends AbstractController{
 
 /**
  * 
- * @Route("/", name="accueil")
+ * @Route("/voyages", name="voyages")
  * @return Response
  */
 public function index(): Response{
-    return $this->render("pages/accueil.html.twig");
+    return $this->render("pages/voyages.html.twig");
       
     }
 }
